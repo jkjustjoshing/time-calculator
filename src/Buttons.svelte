32a1961abf5fbdf6216bf8002a4c3c33ebdb89dc
Josh Kramer <josh.kramer@measuresforjustice.org>
2020-04-06 23:06:33 -0400

    Style buttons in grid

--- src/Buttons.svelte
+++ src/Buttons.svelte
@@ -21,9 +21,26 @@
   }
 </script>
 
-<div>
+<style>
+  .grid {
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+    grid-template-areas:
+      'n-7 n-8 n-9'
+      'n-4 n-5 n-6'
+      'n-1 n-2 n-3'
+      'n-0 . .';
+  }
+  .grid button {
+    color: red;
+  }
+</style>
+
+<div class="grid">
   {#each numbers as number}
-    <button on:click={numberClick}>{number}</button>
+    <button on:click={numberClick} style="grid-area: n-{number};">
+      {number}
+    </button>
   {/each}
   {#each operands as operand}
     <button on:click={() => operandClick(operand)}>{operand.text}</button>
